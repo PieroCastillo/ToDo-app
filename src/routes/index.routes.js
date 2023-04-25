d9eba5fd43749d3fa921b08e2da5a531664bdce4
Abraham ✨ <65505303+aberrospic@users.noreply.github.com>
2023-04-24 19:28:02 -0500

    update routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -9,26 +9,49 @@ router.get("/", async (req, res, next) => {
     res.send(tasks);
 });
 
+router.get("/:title", async (req, res) =>{
+    try{
+        const {title} = req.params
+        const task = await ToDo.findOne({title : title}).lean()
+        console.log(task)
+        res.send(task)
+    }
+    catch(exception){
+
+    }
+})
+
 //Create from db
 router.post("/", async (req, res) => {
     console.log(req.body)
 
     try{
-    const task = new ToDo(req.body);
-    const taskSaved = await task.save()
-    console.log(taskSaved)
-
+        const task = new ToDo(req.body);
+        const taskSaved = await task.save()
+        console.log(taskSaved)
     }catch(error){
         console.log(error)
     }
-
-    
     res.send("it's running")
 });
 
 //Update from db
-router.put("/", (req, res) => {
+router.put("/:title", async (req, res) => {
+    try{
+        const {title} = req.params;
+        const task = await ToDo.findOne({title : title})
+        console.log(task)
+        const {description, done} = req.body
+        task.description = description
+        task.done = done
+        await task.save()
+        console.log(task)
+    }
+    catch(error){
+        console.log(error)
+    }
     res.send("it's running")
+
 });
 
 //Delete from db
